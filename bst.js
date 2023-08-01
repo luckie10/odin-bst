0f5259a51a568f2b2433d4a4e21e5be3fa1f77e3
Boban Kilvin <btrajcevski10@gmail.com>
2023-08-01 03:11:07 -0400

    Add Tree factory

--- bst.js
+++ bst.js
@@ -1,3 +1,13 @@
 const Node = (data, left = null, right = null) => {
   return { data, left, right };
 };
+
+const Tree = (array) => {
+  const root = buildTree(unqArray);
+
+  return {
+    root,
+  };
+};
+
+export { Tree as default };
