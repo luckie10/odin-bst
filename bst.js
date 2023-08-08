2068d74c8b26354ffd00f6b317cef8be66dd636c
Boban Kilvin <btrajcevski10@gmail.com>
2023-08-08 03:59:23 -0400

    Rename some param for clerity, and clean up some code

--- bst.js
+++ bst.js
@@ -35,36 +35,36 @@ const Tree = (array) => {
     }
   };
 
-  const insert = (root, value) => {
-    if (root === null) return Node(value);
+  const insert = (node, value) => {
+    if (node === null) return Node(value);
 
-    if (value < root.data) root.left = insert(root.left, value);
-    else if (value > root.data) root.right = insert(root.right, value);
+    if (value < node.data) node.left = insert(node.left, value);
+    else if (value > node.data) node.right = insert(node.right, value);
 
-    return root;
+    return node;
   };
 
-  const deleteNode = (root, value) => {
-    if (root === null) return root;
+  const deleteNode = (node, value) => {
+    if (node === null) return node;
 
-    if (value < root.data) {
-      root.left = deleteNode(root.left, value);
-      return root;
-    } else if (value > root.data) {
-      root.right = deleteNode(root.right, value);
-      return root;
+    if (value < node.data) {
+      node.left = deleteNode(node.left, value);
+      return node;
+    } else if (value > node.data) {
+      node.right = deleteNode(node.right, value);
+      return node;
     }
 
     // delete if one/both children are empty
     // by returning the opposite child from what we test for
-    if (root.left === null) return root.right;
-    else if (root.right === null) return root.left;
+    if (node.left === null) return node.right;
+    else if (node.right === null) return node.left;
     // delete if both children exist
     else {
       // climb down the left side of the right branch
       // to find lowest number in branch as successor
-      let successorParent = root;
-      let successor = root.right;
+      let successorParent = node;
+      let successor = node.right;
       while (successor.left !== null) {
         successorParent = successor;
         successor = successor.left;
@@ -72,81 +72,77 @@ const Tree = (array) => {
 
       // The successor can only have a right child, therefore if it exist
       // we will replace the successor with its child.
-      if (successor === root) successorParent.right = successor.right;
+      if (successor === node) successorParent.right = successor.right;
       else successorParent.left = successor.right;
 
       // Replace the value of the node we are deleting with the successor value
-      root.data = successor.data;
+      node.data = successor.data;
     }
 
-    return root;
+    return node;
   };
 
-  const find = (root, value) => {
-    if (root === null || root.data === value) return root;
+  const find = (node, value) => {
+    if (node === null || node.data === value) return node;
 
-    if (value < root.data) {
-      return find(root.left, value);
-    } else if (value > root.data) {
-      return find(root.right, value);
+    if (value < node.data) {
+      return find(node.left, value);
+    } else if (value > node.data) {
+      return find(node.right, value);
     }
   };
 
-  const levelOrder = (root, fn = null) => {
-    return levelOrderIterative(root, fn);
-    // return levelOrderRecursive(root, fn);
+  const levelOrder = (node, callback = null) => {
+    return levelOrderIterative(node, callback);
+    // return levelOrderRecursive(node, callback);
   };
 
-  const levelOrderIterative = (root, fn = null) => {
-    if (!root) return;
+  const levelOrderIterative = (node, callback = null) => {
+    if (!node) return;
 
     let queue = [];
     let array = [];
 
-    queue.push(root);
+    queue.push(node);
 
-    while (queue.length > 0) {
-      let first = queue.shift();
-
-      if (fn) fn(first.data);
-      else array.push(first.data);
-
-      if (first.left) queue.push(first.left);
-      if (first.right) queue.push(first.right);
+    while (queue.length) {
+      let firstNode = queue.shift();
+      callback ? callback(firstNode.data) : array.push(firstNode.data);
+      if (firstNode.left) queue.push(firstNode.left);
+      if (firstNode.right) queue.push(firstNode.right);
     }
 
     if (array.length > 0) return array;
   };
 
-  const levelOrderRecursive = (root, fn = null) => {
-    if (!root) return;
+  const levelOrderRecursive = (node, callback = null) => {
+    if (!node) return;
 
     let array = [];
 
-    const height = (root) => {
-      if (!root) return 0;
+    const height = (node) => {
+      if (!node) return 0;
 
-      const lHeight = height(root.left);
-      const rHeight = height(root.right);
+      const lHeight = height(node.left);
+      const rHeight = height(node.right);
 
       if (lHeight > rHeight) return lHeight + 1;
       else return rHeight + 1;
     };
 
-    const currentLevel = (root, level) => {
-      if (!root) return;
+    const currentLevel = (node, level) => {
+      if (!node) return;
       if (level === 1) {
-        if (fn) fn(root.data);
-        else array.push(root.data);
+        callback ? callback(node.data) : array.push(node.data);
       } else if (level > 1) {
-        currentLevel(root.left, level - 1);
-        currentLevel(root.right, level - 1);
+        currentLevel(node.left, level - 1);
+        currentLevel(node.right, level - 1);
       }
     };
 
-    const h = height(root);
+    const h = height(node);
     for (let i = 1; i <= h; i++) {
-      currentLevel(root, i);
+      currentLevel(node, i);
     }
 
     if (array.length > 0) return array;
