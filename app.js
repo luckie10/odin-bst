fc9f4c3a7c55d5cafb3d3ab6b651b49979897d86
Boban Kilvin <btrajcevski10@gmail.com>
2023-08-15 04:59:25 -0400

    Add driver script

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 import Tree from "./bst.js";
 
-const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const bst = Tree(array);
-const bstRoot = bst.root;
-bst.prettyPrint(bstRoot);
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+const generateRandomArray = (size) =>
+  Array.from({ length: size }, generateRandomNumber);
+
+const tree = Tree(generateRandomArray(25));
+
+console.log("Balanced: ", tree.isBalanced());
+
+console.log("Level order:", tree.levelOrder());
+console.log("Pre order:", tree.preorder());
+console.log("Post order:", tree.postorder());
+console.log("In order:", tree.inorder());
+
+for (let i = 0; i < 5; i++) {
+  tree.insert(generateRandomNumber);
+}
+
+console.log("Balanced: ", tree.isBalanced());
+
+console.log("Rebalance", tree.rebalance());
+
+console.log("Balanced: ", tree.isBalanced());
+
+console.log("Level order:", tree.levelOrder());
+console.log("Pre order:", tree.preorder());
+console.log("Post order:", tree.postorder());
+console.log("In order:", tree.inorder());
